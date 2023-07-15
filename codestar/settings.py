b841073c8aa00535fbed2a7fdbee42fddaf42302
Pierre Slagbrand <pierre.slagbrand@hotmail.com>
2023-07-15 21:43:49 +0000

    Debug False

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-pierreslag-djangoblog-vxzg890290u.ws-eu101.gitpod.io']
 ALLOWED_HOSTS = ['ps-django-blog.herokuapp.com', 'localhost']
